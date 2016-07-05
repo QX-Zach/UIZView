07e1ff982e2e8fc8bf809d83228277da8c689287
QX-Zach <767231470@qq.com>
2016-07-05 10:00:12 +0800

    测试修改

--- app/src/main/java/personal/ui/lingchen/uizview/MainActivity.java
+++ app/src/main/java/personal/ui/lingchen/uizview/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        //测试编辑
     }
 
     private void initView() {
