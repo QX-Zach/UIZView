72a8363f59be020c896e16e06aa8df38c8d24911
zach <767231470@ 767231470@ 767231470@qq.com>
2018-06-20 16:55:09 +0800

    完成迷你机温度调节UI，暂时不能自定义设置

--- app/src/main/java/personal/ui/lingchen/uizview/UI/SlideAdjustView.java
+++ app/src/main/java/personal/ui/lingchen/uizview/UI/SlideAdjustView.java
@@ -1,5 +1,6 @@
 package personal.ui.lingchen.uizview.UI;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
@@ -7,24 +8,38 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import java.util.ArrayList;
 
 /**
- * Created by ozner_67 on 2018/6/19.
- * 邮箱：xinde.zhang@cftcn.com
+ * packageName: personal.ui.lingchen.uizview.UI
+ * fileName: SlideAdjustView.java
+ * date: 2018/6/20 15:05
+ * author：zach
+ * Email: xinde.zhang@cftcn.com
+ * <p>
+ * 描述:
  */
 public class SlideAdjustView extends UIZBaseView {
     private static final String TAG = "SlideAdjustView";
     private Paint bgPaint;//背景线画笔
-    private Paint fontPaint;//大背景画笔
+    //    private Paint fontPaint;//大背景画笔
     private Paint dashPaint;//虚线画笔
     private Paint textPaint;//文本画笔
     private Paint numPaint;//数字画笔
-    private Paint thumbPaint;//滑块画笔
+    private Paint thumbPaint;//滑块画笔,大背景画笔
+    private Paint thumbLinePaint;
 
     private int numColor = 0xff5ecffe;//数字颜色
     private int textColor = 0xff5ecffe;//文本颜色
@@ -32,19 +47,73 @@ public class SlideAdjustView extends UIZBaseView {
     private int dashLineColor = 0xff3b71df;//虚线颜色
     private int bgLineColor = 0xff3b71df;//背景线颜色
     private int bgColor = Color.WHITE;//大背景颜色
-//    private boolean isInitParmater = false;
-
-    private int numSize = dpToPx(20);//数字文本大小
-    private int textSize = dpToPx(14);//文字文本大小
+    //    private boolean isInitParmater = false;
+    private String text = "温度调节";
+    private int numSize = dpToPx(15);//数字文本大小
+    private int textSize = dpToPx(18);//文字文本大小
     private float thumRadius = dpToPx(13);//滑块半径
     private float lineOffset = thumRadius / 2 + dpToPx(10);//每条线的偏移量
     private int numHeight, textHeight;
     private int maskOffset = dpToPx(8);
     private MyCircle circleCenter;
-    private int minValue;
-    private int maxValue;
-    private int currentValue;
+    private int minValue = 40;
+    private int maxValue = 99;
+    private int curDelta = 0;
+    private Point thumbPoint = new Point();
+    private Point oldThumbPoint = new Point();
+    private double orgAngle;
+    private int maxDelX = 0;
+    private int targetDelX = 0;
+    private SlideAdjustListener mListener;
+    private boolean isInit = false;
+    private int initValue = Integer.MAX_VALUE;
+//    private boolean isShowAnimator = true;
+//    private ValueAnimator valueAnimator;
 
+//    private void beginAnimator() {
+//        valueAnimator = ValueAnimator.ofInt(curDelta, targetDelX);
+//        valueAnimator.setDuration(1000);
+//        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation) {
+//
+//            }
+//        });
+//    }
+
+    public void setListener(SlideAdjustListener listener) {
+        this.mListener = listener;
+    }
+
+    public void setMinValue(int minValue) {
+        this.minValue = minValue;
+        postInvalidate();
+    }
+
+    public void setMaxValue(int maxValue) {
+        this.maxValue = maxValue;
+        postInvalidate();
+    }
+
+    /**
+     * 设置当前的值
+     *
+     * @param value
+     */
+    public void setValue(int value) {
+        if (value < minValue) {
+            initValue = minValue;
+        } else if (value > maxValue) {
+            initValue = maxValue;
+        } else {
+            initValue = value;
+        }
+
+        if (isInit) {
+            valueToPoint(initValue);
+        }
+    }
 
     public SlideAdjustView(Context context) {
         super(context);
@@ -69,9 +138,6 @@ public class SlideAdjustView extends UIZBaseView {
         bgPaint.setStrokeWidth(4);
         bgPaint.setMaskFilter(new BlurMaskFilter(maskOffset, BlurMaskFilter.Blur.SOLID));
 
-        fontPaint = new Paint();
-        fontPaint.setColor(bgColor);
-
         textPaint = new Paint();
         textPaint.setAntiAlias(true);
         textPaint.setColor(textColor);
@@ -86,6 +152,12 @@ public class SlideAdjustView extends UIZBaseView {
         thumbPaint.setAntiAlias(true);
         thumbPaint.setColor(dashLineColor);
 
+        thumbLinePaint = new Paint();
+        thumbLinePaint.setAntiAlias(true);
+        thumbLinePaint.setColor(bgColor);
+        thumbLinePaint.setStyle(Paint.Style.STROKE);
+        thumbLinePaint.setStrokeWidth(dpToPx(1));
+
         dashPaint = new Paint();
         dashPaint.setAntiAlias(true);
         dashPaint.setColor(dashLineColor);
@@ -97,6 +169,7 @@ public class SlideAdjustView extends UIZBaseView {
         textHeight = getFontHeight(textPaint);
 
         setLayerType(LAYER_TYPE_SOFTWARE, null);
+
     }
 
     private float startAngle;
@@ -106,30 +179,161 @@ public class SlideAdjustView extends UIZBaseView {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        initView();
+    }
+
+
+    /**
+     * 初始化基础参数
+     */
+    private void initView() {
         mHeight = (int) (0.3f * mWidth);
-//        circleCenter = getCircleCenter(new PointF(0, mHeight-numHeight-lineOffset),new PointF(mWidth/2,thumRadius),new PointF(mWidth, mHeight-numHeight-lineOffset));
         circleCenter = getCircleCenter(new PointF(0, thumRadius * 5), new PointF(mWidth / 2, thumRadius), new PointF(mWidth, thumRadius * 5));
         Log.e(TAG, "onSizeChanged: " + circleCenter.toString());
         sweepAngle = calculateAngle(0);
         startAngle = -sweepAngle - 90;
         sweepAngle *= 2;
+
+        //初始化滑块的初始位置
+        thumbPoint.x = mWidth / 2;
+        thumbPoint.y = (int) thumRadius;
+
+        oldThumbPoint.x = thumbPoint.x;
+        oldThumbPoint.y = thumbPoint.y;
+        orgAngle = Math.acos(mWidth / 2 / circleCenter.r);
+        maxDelX = (int) (circleCenter.r * Math.cos(orgAngle));
+        Log.e(TAG, "initView:最大maxDelX--->>>" + maxDelX);
+        isInit = true;
+        if (initValue != Integer.MAX_VALUE) {
+            valueToPoint(initValue);
+        }
     }
 
-
+    /**
+     * 绘制静态背景
+     *
+     * @param canvas
+     */
     private void drawBg(Canvas canvas) {
         canvas.drawCircle(circleCenter.x, circleCenter.y, circleCenter.r, bgPaint);
-        canvas.drawCircle(circleCenter.x, circleCenter.y, circleCenter.r + bgPaint.getStrokeWidth(), fontPaint);
+        thumbPaint.setColor(bgColor);
+        canvas.drawCircle(circleCenter.x, circleCenter.y, circleCenter.r + bgPaint.getStrokeWidth(), thumbPaint);
         canvas.drawCircle(circleCenter.x, circleCenter.y, circleCenter.r - lineOffset, dashPaint);
         Paint tempPaint = new Paint();
-        tempPaint.setShader(new LinearGradient(0, 0, mWidth, 0, new int[]{0xddffffff,0x44ffffff,0x00ffffff,0x44ffffff,0xddffffff}, null, Shader.TileMode.CLAMP));
-        canvas.drawCircle(circleCenter.x,circleCenter.y,circleCenter.r-lineOffset+dpToPx(5),tempPaint);
+        tempPaint.setShader(new LinearGradient(0, 0, mWidth, 0, new int[]{0xddffffff, 0x44ffffff, 0x00ffffff, 0x44ffffff, 0xddffffff}, null, Shader.TileMode.CLAMP));
+        canvas.drawCircle(circleCenter.x, circleCenter.y, circleCenter.r - lineOffset + dpToPx(5), tempPaint);
     }
 
-
+    /**
+     * 绘制滑块,并使滑块旋转
+     *
+     * @param canvas
+     */
     private void drawThumb(Canvas canvas) {
-        canvas.drawCircle(mWidth / 2, thumRadius, thumRadius, thumbPaint);
+        thumbPaint.setColor(dashLineColor);
+        canvas.drawCircle(thumbPoint.x, thumbPoint.y, thumRadius, thumbPaint);
+        canvas.rotate((float) dAngle, thumbPoint.x, thumbPoint.y);
+        Path leftPath = new Path();
+        leftPath.moveTo(thumbPoint.x - dpToPx(2), thumbPoint.y - dpToPx(3));
+        leftPath.lineTo(thumbPoint.x - dpToPx(5), thumbPoint.y);
+        leftPath.lineTo(thumbPoint.x - dpToPx(2), thumbPoint.y + dpToPx(3));
+        canvas.drawPath(leftPath, thumbLinePaint);
+
+        Path rightPath = new Path();
+        rightPath.moveTo(thumbPoint.x + dpToPx(2), thumbPoint.y - dpToPx(3));
+        rightPath.lineTo(thumbPoint.x + dpToPx(5), thumbPoint.y);
+        rightPath.lineTo(thumbPoint.x + dpToPx(2), thumbPoint.y + dpToPx(3));
+        canvas.drawPath(rightPath, thumbLinePaint);
+        canvas.rotate(-(float) dAngle, thumbPoint.x, thumbPoint.y);
     }
 
+    private void drawText(Canvas canvas) {
+        canvas.drawText(String.valueOf(minValue), thumRadius, mHeight - textHeight / 3, numPaint);
+        canvas.drawText(String.valueOf(maxValue), mWidth - getStringWidth(numPaint, String.valueOf(maxValue)) - thumRadius
+                , mHeight - textHeight / 3, numPaint);
+
+        canvas.drawText(text, mWidth / 2 - getStringWidth(textPaint, text) / 2, mHeight / 2 + textHeight / 2, textPaint);
+
+    }
+
+
+    private int pX, pY;
+    private int delX = 0;
+    private int sX = 0;
+    private double dAngle = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                sX = (int) event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //以中间线向两侧计算变化值，只计算X的值，然后转换成角度，间接转换成圆弧上的位置
+                if (delX <= maxDelX && delX >= -maxDelX) {
+                    delX += ((int) event.getX() - sX);
+                    sX = (int) event.getX();
+                }
+                oldThumbPoint.x = thumbPoint.x;
+                oldThumbPoint.y = thumbPoint.y;
+                if (delX > maxDelX) {
+                    delX = maxDelX;
+                }
+                if (delX < -maxDelX) {
+                    delX = -maxDelX;
+                }
+                double delAngle = Math.atan2(delX, circleCenter.r);
+                pX = (int) (mWidth / 2 + Math.sin(delAngle) * circleCenter.r);
+                pY = (int) (thumRadius + circleCenter.r - circleCenter.r * Math.cos(delAngle));
+                dAngle = delAngle * 180 / Math.PI;
+
+                thumbPoint.x = pX;
+                thumbPoint.y = pY;
+                postInvalidate();
+                if (mListener != null) {
+                    mListener.onMove(deltaToValue());
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                thumbPoint.x = pX;
+                thumbPoint.y = pY;
+                if (mListener != null) {
+                    mListener.onFinished(deltaToValue());
+                }
+                break;
+        }
+        return true;
+    }
+
+
+    /**
+     * 值转换成滑块位置
+     *
+     * @param value
+     */
+    private void valueToPoint(int value) {
+        delX = 2 * maxDelX * (value - (maxValue + minValue) / 2) / (maxValue - minValue);
+        double delAngle = Math.atan2(delX, circleCenter.r);
+        pX = (int) (mWidth / 2 + Math.sin(delAngle) * circleCenter.r);
+        pY = (int) (thumRadius + circleCenter.r - circleCenter.r * Math.cos(delAngle));
+        dAngle = delAngle * 180 / Math.PI;
+
+        thumbPoint.x = pX;
+        thumbPoint.y = pY;
+
+        postInvalidate();
+    }
+
+    /**
+     * 滑块位置转换成数值
+     *
+     * @return
+     */
+    private int deltaToValue() {
+
+        return (int) ((maxValue + minValue) / 2.0f + 1.0f * delX / maxDelX * (maxValue - minValue) / 2.0f);
+
+    }
 
     private float calculateAngle(float startX) {
         if (startX < mWidth / 2) {
@@ -145,6 +349,7 @@ public class SlideAdjustView extends UIZBaseView {
         super.onDraw(canvas);
         drawBg(canvas);
         drawThumb(canvas);
+        drawText(canvas);
     }
 
     private MyCircle getCircleCenter(PointF p1, PointF p2, PointF p3)  //求圆心
@@ -179,5 +384,11 @@ public class SlideAdjustView extends UIZBaseView {
                     ", r=" + r +
                     '}';
         }
+    }
+
+    public interface SlideAdjustListener {
+        void onMove(int value);
+
+        void onFinished(int value);
     }
 }
--- app/src/main/java/personal/ui/lingchen/uizview/UIActivity/OznerTempDialActivity.java
+++ app/src/main/java/personal/ui/lingchen/uizview/UIActivity/OznerTempDialActivity.java
@@ -2,29 +2,59 @@ package personal.ui.lingchen.uizview.UIActivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-import butterknife.ButterKnife;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import personal.ui.lingchen.uizview.R;
 import personal.ui.lingchen.uizview.UI.SignalView;
+import personal.ui.lingchen.uizview.UI.SlideAdjustView;
 import personal.ui.lingchen.uizview.UI.UIZOznerTempDial;
 
 public class OznerTempDialActivity extends AppCompatActivity {
-
+    private static final String TAG = "OznerTempDialActivity";
     @BindView(R.id.uiz_OznerTempDial)
     UIZOznerTempDial uizOznerTempDial;
     @BindView(R.id.svSingal)
     SignalView svSingal;
+    @BindView(R.id.sav_adjust)
+    SlideAdjustView savAdjust;
+    @BindView(R.id.tvSildeResult)
+    TextView tvSildeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ozner_temp_dial);
         ButterKnife.bind(this);
+        savAdjust.setListener(new SlideAdjustView.SlideAdjustListener() {
+            @Override
+            public void onMove(final int value) {
+                Log.e(TAG, "onMove: " + value);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvSildeResult.setText(String.valueOf(value));
+                    }
+                });
+            }
 
+            @Override
+            public void onFinished(final int value) {
+                Log.e(TAG, "onFinished: 结束滑动--->>>" + value);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvSildeResult.setText(String.valueOf(value));
+                    }
+                });
+            }
+        });
     }
+
 
     private int currtentValue = 0;
     private int currSignal = 0;
